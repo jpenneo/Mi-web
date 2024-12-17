8aa539ae180bd65bfaff7e46c1eb84f967a3a378
jpenneo <jpenneo@outlook.com>
2024-12-17 18:08:06 +0100

    Listo3

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 
 // Verificar variables DATABASE_URL ESTÉ DEFINIDA
-
-  if (!process.env.DATABASE_URL) {
-    console.error(` Falta la variable de entorno DATABASE_URL`);
-    // Termina la ejecución si falta una variable
-    process.exit(1); 
-  }
-
+if (!process.env.DATABASE_URL) {
+  console.error(`Falta la variable de entorno DATABASE_URL`);
+   // Termina la ejecución si falta una variable
+  process.exit(1);
+ }
 
 // Configurar el servidor
 const app = express();
 
 // Configurar CORS
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' ? process.env.ALLOWED_ORIGIN : '*', // Define dominios específicos en producción
+// Define dominios específicos en producción
+  origin: process.env.NODE_ENV === 'production' ? process.env.ALLOWED_ORIGIN : '*', 
 };
 app.use(cors(corsOptions));
+
+// Configuración de archivos estáticos carpeta 'public'
+app.use(express.static('public')); 
 
 // Configurar la conexión a PostgreSQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //Habilita ssl en producción.
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  // Habilita SSL en producción
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, 
+});
+
+// Ruta para obtener los empleados
+app.get('/empleados', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM empleados'); 
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error al obtener empleados:', error.message);
+    res.status(500).json({ error: 'Error al obtener empleados' });
+  }
 });
 
 // Ruta de prueba para verificar la conexión a la base de datos
-app.get('/', async (req, res) => {
+app.get('/db-status', async (req, res) => { 
   try {
-    // Consulta segura
     const result = await pool.query('SELECT NOW()'); 
     res.json({
       message: 'Conexión exitosa a PostgreSQL',
@@ -43,17 +54,8 @@ app.get('/', async (req, res) => {
     res.status(500).json({ error: 'Error al conectar a la base de datos' });
   }
 });
-
-// Ruta para obtener los empleados
-app.get('/empleados', async (req, res) => {
-  try {
-    // Consulta parametrizada si es necesario
-    const result = await pool.query('SELECT * FROM empleados'); 
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error al obtener empleados:', error.message);
-    res.status(500).json({ error: 'Error al obtener empleados' });
-  }
+app.use((req, res) => {
+  res.status(404).sendFile('404.html', { root: 'public' });
 });
 
 // Configuración del puerto
@@ -61,3 +63,4 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Servidor Express en ejecución en http://localhost:${port}`);
 });
+
