8d7e4505a5231d35cd63bc3b4ca2ecc8f629f7fd
jpenneo <jpenneo@outlook.com>
2025-01-08 13:03:37 +0100

    V0.0.2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { sql } = require('@vercel/postgres');
 
 // Configuración de conexión a PostgreSQL
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
   ssl: { rejectUnauthorized: false }, // Habilita SSL para conexiones seguras
 });
 
